fe5c8e8865a8869998a6ac5c2aad5e8fe271937f
blitz-dev-dotcom <selvamanjula126@gmail.com>
2023-09-28 18:06:50 +0530

    intersection observer modified

--- index.js
+++ index.js
@@ -21,27 +21,6 @@ for(let i=0;i<animation_elements.length;i++){
     observer.observe(el);
 }
 };
-window.addEventListener('DOMContentLoaded',setup2);
-function setup2(){
-    const animation_elements2 = document.querySelectorAll('.animation_on_scroll');
 
-const observer1 = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('animate');
-            
-     }
-        else{
-            entry.target.classList.remove('animate');
-        }
-    })
-},{
-    threshold:0.5
-});
-for(let j=0;j<animation_elements2.length;j++){
-    const el1 = animation_elements2[j];
-    observer1.observe(el1);
-}
-}
 
 
